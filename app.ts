6f6d4b8befdfda3b386b5f123b485ccdc82dff7f
FranciscoArques <103544654+FranciscoArques@users.noreply.github.com>
2024-07-30 22:36:43 -0300

    init commit

---
+++ app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api', (req: Request, res: Response) => {
+  res.send({ status: 'ok', message: 'Health check passed!' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
